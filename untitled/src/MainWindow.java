caf30d0d187b7a86de5707de6be8120ac984b2ad
Florin <105154065+KapyKun@users.noreply.github.com>
2022-10-28 17:31:56 +0300

    Delete untitled directory

--- untitled/src/MainWindow.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class MainWindow {
-    private JButton LoginBtn;
-    private JButton CustomerBtn;
-    private JPasswordField PasswordText;
-    private JTextField UsernameTextField;
-}
