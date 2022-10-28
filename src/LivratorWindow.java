8d00cb4244bede5a22618dbec15eb3b1813f4c0e
KapyKun <haborflorin342@gmail.com>
2022-10-28 17:24:51 +0300

    Form Nou

---
+++ src/LivratorWindow.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class LivratorWindow {
+    private JList ComenziSofer;
+    private JPanel panel1;
+    private JTextField IDComandaTextField;
+    private JButton comandaLivrataButton;
+}
---
+++ src/LogInWindow.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class LogInWindow {
+    private JPanel panel1;
+    private JButton confirmButton;
+    private JCheckBox adminCheckBox;
+    private JCheckBox livratorCheckBox;
+    private JTextField usernameTextField;
+    private JPasswordField passwordPasswordField;
+}
