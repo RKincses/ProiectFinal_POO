7b11fe346ca750f9ca2257399c22961b30a957ee
Florin <105154065+KapyKun@users.noreply.github.com>
2022-10-28 15:31:40 +0300

    Add files via upload

---
+++ untitled/src/MainWindow.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class MainWindow {
+    private JButton LoginBtn;
+    private JButton cancelBtn;
+    private JPasswordField PasswordText;
+    private JTextField UsernameTextField;
+}
---
+++ untitled/src/WindowCustomer.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class WindowCustomer {
+    private JTextField judetTextField;
+    private JTextField judetTextField1;
+    private JTextField localitateTextField1;
+    private JTextField localitateTextField;
+    private JTextField stradaNumarTextField1;
+    private JTextField stradaNumarTextField;
+    private JTextField destinatarTextField;
+    private JTextField expeditorTextField;
+    private JButton finalizareButton;
+}
