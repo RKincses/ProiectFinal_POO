54643e94e6b0db29ac267e1fc4b6906b4bd7c1b7
Florin <105154065+KapyKun@users.noreply.github.com>
2022-10-28 17:31:37 +0300

    Add files via upload

---
+++ ProiectFinal-POO/src/AdminWindow.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class AdminWindow {
+    private JButton alocareComandaButton;
+    private JList ListaComenziNealocate;
+    private JList ListaSoferi;
+    private JTextField textField1;
+    private JTextField textField2;
+}
---
+++ ProiectFinal-POO/src/PlasareComenzi.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class PlasareComenzi {
+    private JTextField numeTextField;
+    private JTextField nrTelefonTextField;
+    private JTextField judetTextField;
+    private JTextField localitateTextField;
+    private JTextField codPostalTextField;
+    private JTextField adresaTextField;
+    private JTextField numeTextField1;
+    private JTextField nrTelefonTextField1;
+    private JTextField judetTextField1;
+    private JTextField localitateTextField1;
+    private JTextField codPostalTextField1;
+    private JTextField adresaTextField1;
+    private JButton confirmaComandaButton;
+    private JButton logInButton;
+}
